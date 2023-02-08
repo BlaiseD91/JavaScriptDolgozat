58f0ff61c678c5e797156b3168a17ba10768de2b
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-02-08 19:49:35 +0100

    javascript funkcionalitás létrehozása

---
+++ src/app.js
@@ -0,0 +1,24 @@
+//console.log("A kapcsolat működik")
+
+const hosszIn = document.querySelector("#hossz");
+const elotolasIn = document.querySelector("#elotolas");
+const fordulatszamIn = document.querySelector("#fordulatszam");
+const calcButton = document.querySelector("#calcButton");
+const eredmenyOut = document.querySelector("#eredmeny");
+
+
+calcButton.addEventListener('click', () => {
+    //console.log("A gomb működik");
+
+    var hossz = parseInt(hosszIn.value);
+    var elotolas = parseInt(elotolasIn.value);
+    var fordulatszam = parseInt(fordulatszamIn.value);
+    var eredmeny = hossz/(elotolas*fordulatszam);
+
+    //console.log(eredmeny);
+    eredmenyOut.value = eredmeny;
+
+    
+});
+
+
